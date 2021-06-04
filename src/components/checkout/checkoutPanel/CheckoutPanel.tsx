d1032bf391b459bea13d7b7897f0fe8de86fc345
RavinduDon <gunarath_se17015@stu.kln.ac.lk>
2021-06-04 10:20:04 +0530

    Project structure created

---
+++ src/components/checkout/checkoutPanel/CheckoutPanel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CheckoutPanel: React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CheckoutPanel;
---
+++ src/components/checkout/priceSection/Price.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Price: React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Price;
