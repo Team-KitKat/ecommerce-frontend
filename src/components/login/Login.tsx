a4c2609dc37f0191969e6c0dc3649880cc12eb99
sankhajay <sankha.rc@gmail.com>
2021-06-05 13:20:33 +0530

    login UI, added Montserrat font

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
 
 const Login: React.FC = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Container fluid={true} className={'login'}>
+      <Row>
+        <Col xs={12} className={'text-center mb-2 login-text '} >
+          LOGO
+        </Col>
+        <Col>
+          <Form>
+            <Form.Group controlId="formUserName" className={'mb-4'}>
+              <Form.Label>Username</Form.Label>
+              <Form.Control required type="text" placeholder=""/>
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control required type="password" placeholder=""/>
+            </Form.Group>
+            <Button type={'submit'} className={'mt-3 float-end login-button'}>Login</Button>
+          </Form>
+        </Col>
+        <Col xs={12} className={'text-center mt-4 fp-text'} >
+          Forget Password
+        </Col>
+      </Row>
+
+    </Container>
+  );
 };
 
 export default Login;
