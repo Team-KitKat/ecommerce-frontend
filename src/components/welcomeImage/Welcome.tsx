b5855690f60033456167d69e2dbc9b53e6a98116
RavinduDon <gunarath_se17015@stu.kln.ac.lk>
2021-06-05 20:42:06 +0530

    Welcome section responsive done

--- src/components/welcomeImage/Welcome.tsx
+++ src/components/welcomeImage/Welcome.tsx
@@ -5,24 +5,20 @@ import LeaveImg from "../../assets/images/leavesLogo.png"
 
 const Welcome: React.FC = () => {
     return (
-        <Container fluid={true} className='welcome-section m-0 p-0'>
-            <Row className='p-0 m-0'>
+            <Row className='welcome-section p-0 m-0'>
                 <Col xs={12} lg={12} sm={12} xl={12} md={12} className='p-0 m-0'>
                     <Image src={BannerImg} alt="Banner Image" className='banner-image'/>
                 </Col>
-            </Row>
-            <Row className='banner-content p-0 m-0'>
-                <Col xs={{offset: 6}} lg={{offset: 7}} md={{offset: 6}} sm={{offset: 6}}>
+                <Col xs={{span: 6,offset: 6}} lg={{span: 5,offset: 7}} md={{span: 6,offset: 6}} sm={{span: 6,offset: 6}} xl={{span: 5,offset: 7}} className='banner-content'>
                     <Image src={LeaveImg} alt='leave image' className='leave-image'/>
                     <br/>
                     <span>100% Healthy & Affordable</span>
                     <h3>Organic Vegetables</h3>
                     <span>Small Changes Big Difference</span>
                     <br/>
-                    <Button>Shop Now</Button>
+                    <Button className='btnShop'>Shop Now</Button>
                 </Col>
             </Row>
-        </Container>
     );
 };
 
