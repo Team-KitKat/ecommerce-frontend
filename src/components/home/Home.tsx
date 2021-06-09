e4b096387a5483b99e4f8b0c07d58a1172cfd198
sankhajay <sankha.rc@gmail.com>
2021-06-09 08:27:16 +0530

    added product UI, position conflict with header banner image

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavigationBar from "../navbar/NavigationBar";
 import Welcome from "../welcomeImage/Welcome";
+import Product from "../productArea/singleProduct/Product";
 
 const Home: React.FC = () => {
   return (
     <div className='shopping-cart'>
       <NavigationBar/>
       <Welcome/>
+      <Product/>
     </div>
   );
 };
--- src/components/productArea/singleProduct/Product.tsx
+++ src/components/productArea/singleProduct/Product.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import {Button, Card, Col, Row} from "react-bootstrap";
+import ProductImg from "../../../assets/images/products/mango.jpeg";
 
 const Product: React.FC = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Card style={{width: '18rem', borderRadius: 10}} border={'secondary'}
+          className={'m-1 pt-5 px-4 mb-5 align-items-center single-product'}>
+      <Card.Img style={{width: '12rem'}} variant="top" src={ProductImg}/>
+      <Card.Body className={'text-right'}>
+        <Card.Title className={'text-center pb-2'}>Product Name</Card.Title>
+        <Card.Text>
+          <Row>
+            <Col xs={8} className={'text-left px-0'}>
+              RS. 65.00
+            </Col>
+            <Col xs={4} className={'text-right px-0'}>
+              RS. 65.00afa
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6} className={'text-left px-0'}>
+              RS. 65.00
+            </Col>
+            <Col xs={6} className={'text-right px-0'}>
+              <Button variant="primary">Add to Cart</Button>
+            </Col>
+          </Row>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Product;
