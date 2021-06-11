2dc7bc31e525d408a5049638a5367bcf73e2f5d4
RavinduDon <gunarath_se17015@stu.kln.ac.lk>
2021-06-11 11:50:11 +0530

    File conflict resolved

--- src/components/CartPreview/EmptyCart/EmptyCart.tsx
+++ src/components/CartPreview/EmptyCart/EmptyCart.tsx
@@ -5,14 +5,14 @@ import EmptyImg from "../../../assets/images/empty_cart.png";
 const EmptyCart: React.FC = () => {
 
     return (
-                <Popover id="popover-contained" className='empty-cart'>
-                    <Popover.Title as="h3">Your Cart is Empty</Popover.Title>
-                    <Popover.Content>
-                        <Image className='empty-image' src={EmptyImg} />
-                        <Button className='bg-info'>Lets Start Shopping</Button>
-                        <strong>Please add items here!!</strong>
-                    </Popover.Content>
-                </Popover>
+        <Popover id="popover-contained" className='empty-cart'>
+            <Popover.Title as="h3">Your Cart is Empty</Popover.Title>
+            <Popover.Content>
+                <Image className='empty-image' src={EmptyImg} />
+                <Button className='bg-info'>Lets Start Shopping</Button>
+                <strong>Please add items here!!</strong>
+            </Popover.Content>
+        </Popover>
 
 
     );
